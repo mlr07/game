8c3360abe89ffa6ed48b85044db2cecf80f6e492
matt rhoads <mrhoads1@gmail.com>
2021-11-28 17:52:46 -0600

    moving shape class to own module

---
+++ shape.py
@@ -0,0 +1,21 @@
+import arcade
+
+
+class Shape(arcade.Sprite):
+    '''extended class for shapes in illusion'''
+
+    def update(self):
+        '''move the sprite and check for out of bounds'''
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH-1:
+            self.right = SCREEN_WIDTH-1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT-1:
+            self.top = SCREEN_HEIGHT-1
+
