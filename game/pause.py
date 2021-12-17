c37ee6e66982967af1e3742733a7b91f7fec94df
matt rhoads <mrhoads1@gmail.com>
2021-12-17 06:40:05 -0600

    added graceful exit to pause

--- game/pause.py
+++ game/pause.py
@@ -14,7 +14,7 @@ class Pause(arcade.View):
         arcade.set_background_color(arcade.color.GRAY)
         arcade.draw_text("PAUSED", self.width*0.5, self.height*0.5+50, (0,0,0), 18, anchor_x="center")
         arcade.draw_text("Hit ESC to return", self.width*0.5, self.height*0.5, (0,0,0), 16, anchor_x="center")
-        arcade.draw_text("Hit ENTER to quit", self.width*0.5, self.height*0.5-50, (0,0,0), 16, anchor_x="center")
+        arcade.draw_text("Hit Q to quit", self.width*0.5, self.height*0.5-50, (0,0,0), 16, anchor_x="center")
         arcade.draw_text("Hit R to start over", self.width*0.5, self.height*0.5-100, (0,0,0), 16, anchor_x="center")
 
 
@@ -22,9 +22,8 @@ class Pause(arcade.View):
         if key == arcade.key.ESCAPE:
             self.window.show_view(self.game_view)
 
-        elif key == arcade.key.ENTER:
-            game = GameView()
-            self.window.show_view(game)
+        elif key == arcade.key.Q:
+            arcade.exit()
 
         elif key == arcade.key.R:
             self.start = start.StartView()
