380cb1921726ebcf5b029ee99e8d32624b01302c
matt rhoads <mrhoads1@gmail.com>
2021-12-21 12:12:12 -0600

    notes for translation

--- game/motion_binding.py
+++ game/motion_binding.py
@@ -4,7 +4,7 @@ import shape
 
 BLUE_SQUARE_SCALE = 1
 GRAY_SQUARE_SCALE = 1.6
-ANGLE_SPEED = 0.75
+SPEED = 0.75
 
 TITLE = "Motion Binding"
 CONTROL_MOVE = "Hold A or D to rotate blue lines"
@@ -29,7 +29,7 @@ class MotionBinding(arcade.View):
 
         img_blue = "./resources/blue_outline.png"
         self.b_square = arcade.Sprite(img_blue , BLUE_SQUARE_SCALE)
-        self.b_square.center_x = self.width*0.5
+        self.b_square.center_x = self.width*0.5-10
         self.b_square.center_y = self.height*0.5
         self.b_square.angle = 45
         self.sprite_list.append(self.b_square)
@@ -63,12 +63,15 @@ class MotionBinding(arcade.View):
         self.sprite_list.update()
 
 
+    # need to us pos attribute (x,y)
+    # calculate xy position of outline center 10px from window center
+    # distance formula or circle geometry
+    # constant motion or A/D key control
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.A:
-            self.sprite_list[0].change_angle = ANGLE_SPEED
+            pass
         elif key == arcade.key.D:
-            self.sprite_list[0].change_angle = -ANGLE_SPEED
-
+            pass
         elif key == arcade.key.W:
             pass
         elif key == arcade.key.S:
@@ -78,9 +81,9 @@ class MotionBinding(arcade.View):
 
     def on_key_release(self, key, key_modifiers):
         if key == arcade.key.A:
-            self.sprite_list[0].change_angle = 0
+            pass
         elif key == arcade.key.D:
-            self.sprite_list[0].change_angle = 0
+            pass
 
 
 def main():
